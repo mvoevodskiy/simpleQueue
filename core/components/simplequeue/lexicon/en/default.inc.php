8e4b01d92c8d7644c0557b636db702156fada027
Nikolay Savin <info@megawebs.kz>
2022-09-19 16:32:19 +0600

    дополнил и перевел английский лексикон

--- core/components/simplequeue/lexicon/en/default.inc.php
+++ core/components/simplequeue/lexicon/en/default.inc.php
@@ -3,32 +3,33 @@
 include_once 'setting.inc.php';
 
 $_lang['simplequeue'] = 'simpleQueue';
-$_lang['simplequeue_menu_desc'] = 'A sample Extra to develop from.';
-$_lang['simplequeue_intro_msg'] = 'You can select multiple items by holding Shift or Ctrl button.';
+$_lang['simplequeue_menu_desc'] = 'Task queue management.';
+$_lang['simplequeue_intro_msg'] = 'You can select multiple tasks at once using Shift or Ctrl.';
 
 $_lang['simplequeue_items'] = 'Tasks';
-$_lang['simplequeue_item_id'] = 'Id';
-$_lang['simplequeue_item_name'] = 'Name';
-$_lang['simplequeue_item_description'] = 'Description';
-$_lang['simplequeue_item_active'] = 'Active';
+$_lang['simplequeue_message_id'] = 'Id';
+$_lang['simplequeue_message_service'] = 'Service';
+$_lang['simplequeue_message_action'] = 'Action';
+$_lang['simplequeue_message_subject'] = 'Subject';
+$_lang['simplequeue_message_createdon'] = 'Time of creation';
+$_lang['simplequeue_message_finishedon'] = 'Completion time';
+$_lang['simplequeue_message_createdby'] = 'Author';
+$_lang['simplequeue_message_processed'] = 'Processed';
+$_lang['simplequeue_message_processing'] = 'Processing';
 
+$_lang['simplequeue_message_enable'] = 'Complete task';
+$_lang['simplequeue_messages_enable'] = 'Complete tasks';
+$_lang['simplequeue_message_disable'] = 'Open task';
+$_lang['simplequeue_messages_disable'] = 'Open tasks';
 
-$_lang['simplequeue_item_update'] = 'Update task';
-$_lang['simplequeue_item_enable'] = 'Enable task';
-$_lang['simplequeue_items_enable'] = 'Enable tasks';
-$_lang['simplequeue_item_disable'] = 'Disable task';
-$_lang['simplequeue_items_disable'] = 'Disable tasks';
-$_lang['simplequeue_item_remove'] = 'Remove task';
-$_lang['simplequeue_items_remove'] = 'Remove tasks';
-$_lang['simplequeue_item_remove_confirm'] = 'Are you sure you want to remove this task?';
-$_lang['simplequeue_items_remove_confirm'] = 'Are you sure you want to remove this tasks?';
-
-$_lang['simplequeue_item_err_name'] = 'You must specify the name of task.';
-$_lang['simplequeue_item_err_ae'] = 'A task already exists with that name.';
-$_lang['simplequeue_item_err_nf'] = 'Task not found.';
-$_lang['simplequeue_item_err_ns'] = 'Task not specified.';
-$_lang['simplequeue_item_err_remove'] = 'An error occurred while trying to remove the task.';
-$_lang['simplequeue_item_err_save'] = 'An error occurred while trying to save the task.';
+$_lang['simplequeue_message_err_name'] = 'You must provide a name for the Task.';
+$_lang['simplequeue_message_err_ae'] = 'An object with the same name already exists.';
+$_lang['simplequeue_message_err_nf'] = 'Task not found.';
+$_lang['simplequeue_message_err_ns'] = 'Task not specified.';
+$_lang['simplequeue_message_err_remove'] = 'Error while deleting Task.';
+$_lang['simplequeue_message_err_save'] = 'Error when saving Task.';
 
 $_lang['simplequeue_grid_search'] = 'Search';
 $_lang['simplequeue_grid_actions'] = 'Actions';
+
+$_lang['simplequeue_service_nf'] = 'simpleQueue service not found';
